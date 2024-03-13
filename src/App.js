5c8c8c43da47addc5c0745c329bddcf0baa6566b
Nam Le <lenam28082000@gmail.com>
2024-03-13 12:46:06 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Router>
           <Header />
             <Routes>
-                <Route path="/React-MovieApp" element={<Home />}></Route>
+                <Route index element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
